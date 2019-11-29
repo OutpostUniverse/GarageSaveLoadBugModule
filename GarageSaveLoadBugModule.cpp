c2e8f17974ac732adc0c19a68b952ae8b95615ef
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-11-29 01:00:29 -0700

    Add code patch to offset unit array allocation

--- GarageSaveLoadBugModule.cpp
+++ GarageSaveLoadBugModule.cpp
@@ -12,13 +12,45 @@
 // make
 
 
+#include <cstddef>
+#include <cstdint>
+#include "OP2Memory.h"
+#include "Log.h"
+#include "Log/LoggerMessageBox.h"
+#include "StringConversion.h"
+
+
+void* AlignedAlloc(std::size_t allocSize);
+
+
+LoggerMessageBox logger;
+
+
 #define EXPORT extern "C" __declspec(dllexport)
 
 
-EXPORT void mod_init()
-{
+EXPORT void mod_init() {
+	SetLoggerDebug(&logger);
+	SetLoggerMessage(&logger);
+	SetLoggerError(&logger);
+
+	if (!EnableOp2MemoryPatching()) {
+		LogError("GarageSaveLoadBugModule: Failed to enable memory patching");
+	}
+	if (!Op2RelinkCall(0x00435665, reinterpret_cast<const void*>(&AlignedAlloc))) {
+		LogError("GarageSaveLoadBugModule: Failed to patch unit array memory allocator");
+	}
 }
 
-EXPORT void mod_destroy()
-{
+
+__cdecl void* AlignedAlloc(std::size_t allocSize) {
+	constexpr std::uintptr_t OriginalAllocAddress = 0x004C21F0;
+	auto allocFunction = reinterpret_cast<decltype(&AlignedAlloc)>(OriginalAllocAddress);
+
+	auto result = reinterpret_cast<std::uintptr_t>(allocFunction(allocSize + 120));
+	if (result != 0) {
+		LogError("Address: " + AddrToHexString(result) + "  Alignment: " + AddrToHexString(result % 120) + "  Offset: " + AddrToHexString(120 - (result % 120)) + "  Result: " + AddrToHexString(result + (120-(result % 120))));
+		result += (120 - (result % 120));
+	}
+	return reinterpret_cast<void*>(result);
 }
